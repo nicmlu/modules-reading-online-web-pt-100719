3dab43a88549c0b475832d204ae4787a0e5a4332
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 09:37:15 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,6 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer 
-  extend FancyDance::ClassMethods 
-  include FancyDance::InstanceMethods
-  
   attr_reader :name 
   
   def initialize(name)

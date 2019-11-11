e12f009a86ebf79a8ab2b72170c8198df9d40bb7
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 01:17:10 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  extend Metadancing
+  extend MetaDancing
   include Dance
   attr_reader :name 
   

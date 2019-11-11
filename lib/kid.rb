0358b75186bfc104bd8528cd0b9bb5f377131f25
Nicole Lucien <nicolemlucien@gmail.com>
2019-11-11 02:08:46 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid 
-  extend MetaDancing
-  include Dance
+  extend Fancy_Dance :: ClassMethods 
+  include Fancy_Dance :: InstanceMethods
   attr_reader :name 
   
   def initialize(name)
